0b8f1e9d9e32c05a5e628fecb9113a1f4883441a
katiehan22 <katiehan22@gmail.com>
2022-10-31 15:33:38 -0700

    Add products controller

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,48 @@
 class Api::ProductsController < ApplicationController
+  def index 
+    case params[:category]
+      when "womens"
+        case params[:subcategory]
+          when "undefined"
+            @products ||= Product.where(category: "womens")
+          when "leggings"
+            @products ||= Product.where(category: "womens", subcategory: "leggings")
+          when "hoodies"
+            @products ||= Product.where(category: "womens", subcategory: "hoodies")
+          when "joggers"
+            @products ||= Product.where(category: "womens", subcategory: "joggers")
+          when "shirts"
+            @products ||= Product.where(category: "womens", subcategory: "shirts")
+        end
+      when "mens"
+        case params[:subcategory]
+          when "undefined"
+            @products ||= Product.where(category: "mens")
+          when "joggers"
+            @products ||= Product.where(category: "mens", subcategory: "joggers")
+          when "hoodies"
+            @products ||= Product.where(category: "mens", subcategory: "hoodies")
+          when "shorts"
+            @products ||= Product.where(category: "mens", subcategory: "shorts")
+          when "shirts"
+            @products ||= Product.where(category: "mens", subcategory: "shirts")
+        end
+      when "accessories"
+        case params[:subcategory]
+          when "undefined"
+            @products ||= Product.where(category: "accessories")
+          when "belt-bags"
+            @products ||= Product.where(category: "accessories", subcategory: "belt-bags")
+          when "backpacks"
+            @products ||= Product.where(category: "accessories", subcategory: "backpacks")
+        end
+    end
+    
+    render :index
+  end
+
+  def show 
+    @product = Product.find_by(id: params[:id])
+    render :show 
+  end
 end
---
+++ app/views/api/products/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @product, :id, :name, :price, :category, :subcategory, :details, :description, :colours, :sizes 
