731b502a5ea175418f89d94c6064b4d50c029081
katiehan22 <katiehan22@gmail.com>
2022-11-01 15:33:26 -0700

    Add mens and accessories to main nav

--- frontend/src/components/MainNavigation/index.js
+++ frontend/src/components/MainNavigation/index.js
@@ -24,8 +24,26 @@ function MainNavigation() {
               </ul>
             </div>
           </li>
-          <li id="mens-li"><NavLink to="#">MEN</NavLink></li>
-          <li id="accessories-li"><NavLink to="#">ACCESSORIES</NavLink></li>
+          <li id="mens-li"><NavLink to="#">MEN</NavLink>
+            <div className='mens-dropdown'>
+              <ul className='mens-list'>
+                <li className='category-li'><NavLink to="#">MEN'S CLOTHES</NavLink></li>
+                <li className='subcategory-li'><NavLink to="#">Joggers</NavLink></li>
+                <li className='subcategory-li'><NavLink to="#">Hoodies & Sweatshirts</NavLink></li>
+                <li className='subcategory-li'><NavLink to="#">Shorts</NavLink></li>
+                <li className='subcategory-li'><NavLink to="#">Shirts</NavLink></li>
+              </ul>
+            </div>
+          </li>
+          <li id="accessories-li"><NavLink to="#">ACCESSORIES</NavLink>
+            <div className='accessories-dropdown'>
+              <ul className='accessories-list'>
+                <li className='category-li'><NavLink to="#">ACCESSORIES</NavLink></li>
+                <li className='subcategory-li'><NavLink to="#">Belt-Bags</NavLink></li>
+                <li className='subcategory-li'><NavLink to="#">Backpacks</NavLink></li>
+              </ul>
+            </div>
+          </li>
         </ul>
       </nav>
     </div>
