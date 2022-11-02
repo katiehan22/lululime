939ad48cb11e25f859607f05d0d1c56716e968ac
katiehan22 <katiehan22@gmail.com>
2022-11-01 18:18:12 -0700

    Add styling to product details page

--- frontend/src/components/ProductDetailPage/index.js
+++ frontend/src/components/ProductDetailPage/index.js
@@ -1,8 +1,9 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import { fetchProduct } from "../../store/products";
 import "./ProductDetailPage.css";
+import testImg from '../../assets/images/product-item-test.png';
 
 const ProductDetailPage = () => {
   const dispatch = useDispatch();
@@ -16,7 +17,29 @@ const ProductDetailPage = () => {
   if(product) {
     return (
       <>
-        {product.name}
+        <div className="product-details-page">
+          <div className="product-details-top">
+            <div className="product-details-img">
+              <img src={testImg}/>
+            </div>
+
+            <div className="product-details-top-middle">
+              <div className="breadcrumbs">
+                <Link className="breadcrumb-link" to={`/products/${product.category}`}>{product.category}</Link> / <Link className="breadcrumb-link" to={`/products/${product.category}/${product.subcategory}`}>{product.subcategory}</Link>
+              </div>
+              <div className="product-details-name">
+                {product.name}
+              </div>
+              <div className="product-details-price">
+                <h3>${product.price} USD</h3>
+              </div>
+            </div>
+
+            <div className="product-details-recommended-top">
+              <h2>You may like</h2>
+            </div>
+          </div>
+        </div>
       </>
     )
   } else {
