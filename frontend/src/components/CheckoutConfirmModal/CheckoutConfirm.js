e9ee251f8386f10df2f2b141ef339645789d2ff4
katiehan22 <katiehan22@gmail.com>
2022-12-05 15:25:51 -0800

    change checkout confirmation

--- frontend/src/components/CheckoutConfirmModal/CheckoutConfirm.js
+++ frontend/src/components/CheckoutConfirmModal/CheckoutConfirm.js
@@ -16,13 +16,18 @@ const CheckoutConfirmation = ({ setShowCheckoutModal }) => {
       </div>
       <div className="checkout-confirmation-container">
         <div className="checkout-confirmation-header">
-          <h2>Thanks for your order!</h2>
+          <h2>Thanks for checking out my site!</h2>
+          <h3>Check out my other work:</h3>
         </div>
-        <div className="continue-shopping-button-container">
+        <div className="icon-container">
+          <a href="https://github.com/katiehan22/lululime" target="_blank"><i id="github-icon" class="fa-brands fa-square-github"></i></a>
+          <a href="https://www.linkedin.com/in/katiehan22/" target="_blank"><i id="linkedin-icon" class="fa-brands fa-linkedin"></i></a>
+        </div>
+        {/* <div className="continue-shopping-button-container">
           <button id="continue-shopping-button" onClick={() => handleClick()}>
             Continue Shopping
           </button>
-        </div>
+        </div> */}
       </div>
     </>
   )
