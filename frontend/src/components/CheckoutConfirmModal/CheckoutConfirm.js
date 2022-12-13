86d30333b8535ce4f4d8b1b954c4a1de5664d277
katiehan22 <katiehan22@gmail.com>
2022-12-12 16:09:17 -0800

    add portfolio link

--- frontend/src/components/CheckoutConfirmModal/CheckoutConfirm.js
+++ frontend/src/components/CheckoutConfirmModal/CheckoutConfirm.js
@@ -22,6 +22,7 @@ const CheckoutConfirmation = ({ setShowCheckoutModal }) => {
         <div className="icon-container">
           <a href="https://github.com/katiehan22/lululime" target="_blank"><i id="github-icon" class="fa-brands fa-square-github"></i></a>
           <a href="https://www.linkedin.com/in/katiehan22/" target="_blank"><i id="linkedin-icon" class="fa-brands fa-linkedin"></i></a>
+          <a href="https://katiehan.netlify.app/" target="_blank"><i class="fa-solid fa-folder" id="portfolio-icon"></i></a>
         </div>
         {/* <div className="continue-shopping-button-container">
           <button id="continue-shopping-button" onClick={() => handleClick()}>
